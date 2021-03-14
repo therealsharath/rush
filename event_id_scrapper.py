1f66a77313ddb83c1f242c1965a55264808cac13
therealsharath <palathingal@gatech.edu>
2021-03-14 07:04:34 -0400

    Stored parsed data into a csv file

--- event_id_scrapper.py
+++ event_id_scrapper.py
@@ -1,13 +1,19 @@
 from re import T
 import requests
 from bs4 import BeautifulSoup as bs
+import pandas as pd
 
 page = requests.get("http://ufcstats.com/statistics/events/completed?page=all")
 parsed_content = bs(page.content, 'html.parser')
 results = parsed_content.find_all('a', class_='b-link b-link_style_black', href=True)
 
+event_id_data = {}
+
 for result in results:
-    to_store = {}
-    to_store['event_id'] = result['href'].split('/')[-1]
-    to_store['event_name'] = result.text.strip()
-    print(to_store)
+    to_store = []
+    to_store.append(result['href'].split('/')[-1])
+    to_store.append(result.text.strip())
+    event_id_data[len(event_id_data) + 1] = to_store
+
+event_ids = pd.DataFrame.from_dict(event_id_data, orient='index', columns=['event_id', 'event_name'])
+event_ids.to_csv('./data/raw_data/event_ids.csv')
