10a38de249a4f118013a5b2b9c3d2783e83e7529
therealsharath <palathingal@gatech.edu>
2021-03-28 20:14:45 -0400

    Changed data compiler to compile data

--- data_compiler.py
+++ data_compiler.py
@@ -1,11 +1,12 @@
 import pandas as pd 
 
+fighters = pd.read_csv('./data/raw_data/fighter_details.csv')
+fights = pd.read_csv('./data/raw_data/combined_fights.csv')
 
 def prepareData(fighter):
-    fighters = pd.read_csv('./data/raw_data/fighter_details.csv')
     fighter_data = fighters.loc[fighters['fighter_name'] == fighter].values.tolist()
     fighter_data = fighter_data[0]
     print(fighter_data)
-    return []
+    return fighter_data
 
 prepareData('Khabib Nurmagomedov')
