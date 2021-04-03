6d621accd8d6ad33104bd366aa19f8f785537db5
therealsharath <palathingal@gatech.edu>
2021-04-03 12:25:41 -0400

    Obtained and sorted fighter's fights based on the the date of the event

--- data_compiler.py
+++ data_compiler.py
@@ -1,12 +1,33 @@
 import pandas as pd 
+from datetime import datetime
 
-fighters = pd.read_csv('./data/raw_data/fighter_details.csv')
-fights = pd.read_csv('./data/raw_data/combined_fights.csv')
+fighters = pd.read_csv('./data/processed_data/cleaned_fighter_details.csv')
+fights = pd.read_csv('./data/processed_data/cleaned_fight_details.csv')
+corners = ['r', 'b']
+
+def compile_fights(fighter):
+    fighter_fight_list = []
+
+    for corner in corners:
+        fighter_fights = fights.loc[fights['fighter_' + corner] == fighter]
+        if len(fighter_fights) > 0:
+            for idx, fight in fighter_fights.iterrows():
+                fight['fight_date'] = datetime.strptime(fight['fight_date'] + ' 00:00:00', '%m/%d/%Y %H:%M:%S')
+                fighter_fight_list.append(fight)    
+    
+    fighter_fight_list = pd.DataFrame(fighter_fight_list)
+    fighter_fight_list.sort_values(by = 'fight_date')
+
+    del fight['Unnamed: 0']
+    return fighter_fight_list
 
 def prepareData(fighter):
     fighter_data = fighters.loc[fighters['fighter_name'] == fighter].values.tolist()
     fighter_data = fighter_data[0]
-    print(fighter_data)
+
+    fighter_fights = compile_fights(fighter)
+
+    print(fighter_fights['fight_date'])
     return fighter_data
 
 prepareData('Khabib Nurmagomedov')
