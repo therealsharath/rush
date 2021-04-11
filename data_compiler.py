81a24fa3c79577f96ee4caee9d9255cf7d1835a5
therealsharath <palathingal@gatech.edu>
2021-04-10 22:44:00 -0400

    Compiling data based on fighter fights

--- data_compiler.py
+++ data_compiler.py
@@ -19,6 +19,7 @@ def compile_fights(fighter):
     fighter_fight_list = fighter_fight_list.sort_values(by = 'fight_date', ascending=False)
 
     del fighter_fight_list['Unnamed: 0']
+
     return fighter_fight_list
 
 def prepareData(fighter):
@@ -27,11 +28,39 @@ def prepareData(fighter):
     fighter_fights = compile_fights(fighter)
     return fighter_fights
 
+def curr_stats(fighter, fights):
+    return []
 
 fight_final_data = []
 #Main processor
 for fighter in fighters.iterrows():
     fighter = fighter[1]
     fighter_fights = prepareData(fighter['fighter_name'])
-    print(fighter_fights['r_att_sig_str'].mean())
+
+    win_stk = 0
+    lose_stk = 0
+    draw_stk = 0
+    counted = 0
+
+    for idx, fight in fighter_fights.iterrows():
+        if counted < 5:
+            counted += 1
+            if fight['winner'] == fighter['fighter_name']:
+                win_stk += 1
+                lose_stk = 0
+                draw_stk = 0
+            elif fight['winner'] == '':
+                win_stk += 0
+                lose_stk = 0
+                draw_stk += 1
+            else:
+                win_stk = 0
+                lose_stk += 1
+                draw_stk = 0   
+        fighter_fights = fighter_fights.drop(idx)
+        stats_for_fight = curr_stats(fighter, fighter_fights)
+        
+    print(win_stk, lose_stk, draw_stk) 
     break
+
+# "_85aaee9fc23ca61102952862a10b544c-scss" 
